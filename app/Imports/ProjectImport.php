1213515888aad913edb81c8cdd746d507e5c7496
Otabek <otabekrahmonovtest@gmail.com>
2024-08-21 18:46:01 +0500

    feat: Validation excel data

--- app/Imports/ProjectImport.php
+++ app/Imports/ProjectImport.php
@@ -5,12 +5,16 @@ namespace App\Imports;
 use App\Models\Type;
 use App\Models\Project;
 use App\Factory\ProjectFactory;
+use App\Models\FailedRow;
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Validators\Failure;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class ProjectImport implements ToCollection, WithHeadingRow
+class ProjectImport implements ToCollection, WithHeadingRow, WithValidation, SkipsOnFailure
 {
     /**
     * @param Collection $collection
@@ -21,7 +25,7 @@ class ProjectImport implements ToCollection, WithHeadingRow
         $typesMap = $this->getTypesMap(Type::all()); 
 
         
-        foreach($collection as $row){
+        foreach($collection as $row){  
             if(!isset($row['naimenovanie'])) continue;
             
             $projectFactory = ProjectFactory::make($typesMap, $row);
@@ -46,6 +50,45 @@ class ProjectImport implements ToCollection, WithHeadingRow
         return $map;
     }
 
-                                                                
+    public function rules(): array
+    {
+        return [
+            'tip' => 'required|string',
+            'naimenovanie' => 'required|string',
+            'data_sozdaniia' => 'required|integer',
+            'podpisanie_dogovora' => 'required|integer',
+            'dedlain' => 'nullable|integer', 
+            'setevik' => 'nullable|string',
+            'nalicie_autsorsinga' => 'nullable|string',
+            'nalicie_investorov' => 'nullable|string',
+            'sdaca_v_srok' => 'nullable|string',
+            'vlozenie_v_pervyi_etap' => 'nullable|integer',
+            'vlozenie_vo_vtoroi_etap' => 'nullable|integer',
+            'vlozenie_v_tretii_etap' => 'nullable|integer',
+            'vlozenie_v_cetvertyi_etap' => 'nullable|integer',
+            'kolicestvo_ucastnikov' => 'nullable|integer',
+            'kolicestvo_uslug' => 'nullable|integer',
+            'kommentarii' => 'nullable|string',
+            'znacenie_effektivnosti' => 'nullable|numeric',
+        ];
+    }   
+    
+    public function onFailure(Failure ...$failures)
+    {
+        $map = [];
+        foreach($failures as $failure){ 
+            foreach($failure->errors() as $error){
+                $map[] = [
+                    'key' => $failure->attribute(),
+                    'row' => $failure->row(),
+                    'message' => $error,
+                    'task_id' => 1,
+                ];
+            }
+        }
+
+        if(count($map)>0) FailedRow::insertFailedRows($map);
+    }   
+    
 
 }
